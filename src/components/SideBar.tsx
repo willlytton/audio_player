204afdd71a0f93983295b4892a4c50852dfed2e0
Will Lytton <maplejwl@gmail.com>
2023-11-28 21:10:27 -0500

    fixed tailwind

---
+++ src/components/SideBar.tsx
@@ -0,0 +1,15 @@
+const SideBar = () => {
+    return (
+        <>
+            <div className="flex flex-col">
+                <div className="flex w-60 bg-black">
+
+                </div>
+            </div>
+
+        </>
+
+    );
+};
+
+export default SideBar;
